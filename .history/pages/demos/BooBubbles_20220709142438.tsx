772c56cfa812db723174f98b3efffe917428b358
Aidan Sunbury <aidansunbury@gmail.com>
2022-07-30 22:58:09 -0700

    making sure everything is updated

---
+++ .history/pages/demos/BooBubbles_20220709142438.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { useFonts } from "expo-font";
+
+export default function BooBubbles() {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{ fontFamily: "Futura", color: "#00B1BB", fontWeight: "600" }}
+      >
+        boo boo
+      </Text>
+      <Text>HOME </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ .history/pages/demos/BooBubbles_20220709144203.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { useFonts } from "expo-font";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+export default function BooBubbles() {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{ fontFamily: "Futura", color: "#00B1BB", fontWeight: "600" }}
+      >
+        boo boo
+      </Text>
+      <Text>HOME </Text>
+      <YoutubePlayer
+        height={200}
+        width={300}
+        videoId={"iee2TATGMyI"}
+        play={true}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
